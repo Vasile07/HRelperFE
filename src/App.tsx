098fdb55ab4996ff560f4a42269a4ba9e005c216
Patrick Radu <patrick_munteanu@yahoo.com>
2026-04-27 14:10:40 +0300

    Login Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import AddNewJobPost from "./pages/AddNewJobPost.tsx";
 import JobDetails from "./pages/JobDetails.tsx";
+import LoginPage from "./components/LoginPage.tsx";
 
 function App() {
   return (
       <div style={{flex: 1, backgroundColor: "#fff"}}>
         <Routes>
+            <Route path="/Login" element={<LoginPage/>}/>
             <Route path="/AddJobPost" element={<AddNewJobPost/>}/>
             <Route path="/JobDetails/:jobId" element={<JobDetails/>}/>
         </Routes>
