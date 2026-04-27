dfeaa4a7924acec4910f20f83cf3b5f7d87e775d
Patrick Radu <patrick_munteanu@yahoo.com>
2026-04-27 14:21:31 +0300

    Job viewer

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import AddNewJobPost from "./pages/AddNewJobPost.tsx";
 import JobDetails from "./pages/JobDetails.tsx";
 import LoginPage from "./components/LoginPage.tsx";
+import JobViewer from "./components/JobViewer.tsx";
 
 function App() {
   return (
       <div style={{flex: 1, backgroundColor: "#fff"}}>
         <Routes>
             <Route path="/Login" element={<LoginPage/>}/>
+            <Route path="/JobViewer" element={<JobViewer/>}/>
             <Route path="/AddJobPost" element={<AddNewJobPost/>}/>
             <Route path="/JobDetails/:jobId" element={<JobDetails/>}/>
         </Routes>
