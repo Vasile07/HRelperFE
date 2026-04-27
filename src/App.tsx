41ba3ea93c49e48512ab4522a6e92411a4015128
Patrick Radu <patrick_munteanu@yahoo.com>
2026-04-27 14:10:40 +0300

    Login Page

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import AddNewJobPost from "./pages/AddNewJobPost.tsx";
 import JobDetails from "./pages/JobDetails.tsx";
 import RegisterNewAccount from "./pages/RegisterNewAccount.tsx";
 import DiscoverJobPageDashboard from "./pages/DiscoverJobPageDashboard.tsx";
+import LoginPage from "./components/LoginPage.tsx";
 
 function App() {
   return (
       <div style={{flex: 1, backgroundColor: "#fff"}}>
         <Routes>
+            <Route path="/Login" element={<LoginPage/>}/>
             <Route path="/AddJobPost" element={<AddNewJobPost/>}/>
             <Route path="/JobDetails/:jobId" element={<JobDetails/>}/>
             <Route path="/Register" element={<RegisterNewAccount/>}/>
